123f5b538e7298a5e2f2e7af509db5360719ad93
DESKTOP-7RTB3KO\zhiming <liaozhimingandy@qq.com>
2023-05-11 23:38:26 +0800

    1.调整部分内容;

--- lib/pages/me/me_page.dart
+++ lib/pages/me/me_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:welinkapp/pages/const.dart';
+import 'package:welinkapp/pages/components/UserIconWidget.dart';
 import 'package:welinkapp/pages/discover/discover_cell.dart';
 
 class MePage extends StatefulWidget {
@@ -12,72 +12,54 @@ class MePage extends StatefulWidget {
 class MePageState extends State<MePage>
     with AutomaticKeepAliveClientMixin<MePage> {
 
-   // 头像组件
-  Widget _headerCell() {
-    /// 头部组件
-    return Container(
-      height: 200,
-      color: Colors.blue,
-      child: Container(
-        margin: const EdgeInsets.only(top: 100),
-        padding: const EdgeInsets.all(10),
-        child: Container(
-          child: Row(
-            children: [
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: const DecorationImage(
-                      image: AssetImage('assets/images/more.png')),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Row(
-                          children: const [
-                            Text(
-                              'Franky',
-                              style: TextStyle(fontSize: 28),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            Text(
-                              '昵称',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.grey),
-                            ),
-                            Image(
-                              image: AssetImage('assets/images/more.png'),
-                              width: 15,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+  Widget buildRow(icon, title, isEnd) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+            UserIconWidget(
+              padding: const EdgeInsets.only(top: 0.0, right: 14.0, left: 14.0),
+              width: 22.0,
+              height: 22.0,
+              image: icon,
+              isNetwork: false,
+              onPressed: () {
+                // NavigatorUtils.goPerson(context, eventViewModel.actionUser);
+              }),
+          Expanded(
+            child: Container(
+              decoration: !isEnd
+                  ? const BoxDecoration(
+                       border: Border(
+                          bottom:
+                              BorderSide(color: Color(0xffd9d9d9), width: .3)))
+                  : null,
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 15.0),
+                    child: Text(
+                      title,
+                      style: const TextStyle(fontSize: 15.0),
+                    ),
                   ),
-                ),
+                  Container(
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 15.0, right: 10.0),
+                        child: const Icon(
+                          Icons.chevron_right,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
+        ],
+      );
+    } 
 
   @override
   bool get wantKeepAlive => true;
@@ -86,48 +68,39 @@ class MePageState extends State<MePage>
   Widget build(BuildContext context) {
     super.build(context);
     
-    return Scaffold(
-      body: Container(
-        child: Stack(
-        children: [
-          Container(
-            color: themeColor,
-            child: MediaQuery.removePadding(
-                context: context,
-                removeTop: true,
-                child: ListView(
-                  children: [
-                    _headerCell(),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    const DiscoverCell(
-                        title: '收藏', imageName: 'images/icon_right.png'),
-                    
-                  ],
-                )),
-          ),
-          Container(
-              margin: const EdgeInsets.only(top: 40),
-              height: 25,
-              child: Container(
-                margin: const EdgeInsets.only(right: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      child: const Image(
-                        image: AssetImage('assets/images/more.png'),
-                      ),
-                      onTap: () {
-                        print('点击了拍照');
-                      },
-                    )
-                  ],
+    return  Stack(
+      children: [
+        ListView(
+          children: [
+            Container(
+               height: 180.0,
+               color: Colors.blue,
+               child: 
+                  RawMaterialButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                         MaterialPageRoute(
+                            builder: (c) {
+                              return const DiscoverCell(title: 'test', imageName: '',);
+                            },
+                        ),
+                      );
+                }
+               ),
+            ), // 头像
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 18.0,
                 ),
-              )),
-        ],
-      ),
-    ));
+                buildRow('assets/images/more.png', '设置', true),
+              ],
+            ) // 设置
+          ],
+        )
+      ],
+    );
   }
 }
