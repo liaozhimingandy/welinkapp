2051b4de26bddcec385dd5ae527adac44516e1f5
liaozhiming <liaozhimingandy@qq.com>
2023-05-23 18:27:12 +0800

    1.测试通道;

--- lib/core/im.dart
+++ lib/core/im.dart
@@ -4,7 +4,7 @@ import 'manager/im_manager.dart';
 
 class IM {
   static const version = '1.0';
-  static const _channel =  MethodChannel('com.alsoapp.welink');
+  static const _channel = MethodChannel('abc');
   static final iMManager = IMManager(_channel);
   IM._();
 }
--- lib/core/manager/im_manager.dart
+++ lib/core/manager/im_manager.dart
@@ -1,43 +1,53 @@
-
 import 'package:flutter/foundation.dart';
+import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:welinkapp/core/controller/im_manager_controller.dart';
 
-class IMManager implements IIMManager{
-  final MethodChannel _channel;
+class IMManager implements IIMManager {
+  MethodChannel _channel;
 
-  IMManager(this._channel){
+  IMManager(this._channel) {
     _addNativeCallback(_channel);
   }
-  
-  void _addNativeCallback(MethodChannel _channel) {
-    _channel.setMethodCallHandler((call) {
-      try{
-        debugPrint('$call.method');
-        String type = call.arguments['type'];
-         switch (type) {
-              case 'initSDK':
-                break;
-         }
-        }catch(error, stackTrace){
-          debugPrint('回调失败了。${call.method} ${call.arguments['type']} ${call.arguments['data']} $error $stackTrace');
-        }
-      return Future.value(null);
-    });
-  }
 
-@override
-Future<dynamic> initSDK(){
-  return _channel.invokeMethod(
-        'initSDK',
-        {'uid': 123}
-  );
-}
+  // try {
+  //   debugPrint('$call');
+  //   String type = call.arguments['type'];
+  //   switch (call.method) {
+  //     case 'initSDK':
+  //       break;
+  //   }
+  // } catch (error, stackTrace) {
+  //   debugPrint(
+  //       '回调失败了。${call.method} ${call.arguments['type']} ${call.arguments['data']} $error $stackTrace');
+  // }
+  // return Future.value(null);
+
+  @override
+  Future<dynamic> initSDK() {
+    return _channel.invokeMethod('getBatteryLevel');
+    // return Future.value(null);
+  }
 
   @override
   Future<dynamic> uninitSDK() {
-     return _channel.invokeMethod('unInitSDK', {});
+    return _channel.invokeMethod('unInitSDK', {});
   }
 
-
+  void _addNativeCallback(MethodChannel channel) {
+    _channel.setMethodCallHandler((call) {
+      try {
+        debugPrint('$call');
+        String type = call.arguments['type'];
+        switch (call.method) {
+          case 'initSDK':
+            break;
+        }
+      } catch (error, stackTrace) {
+        debugPrint(
+            '回调失败了。${call.method} ${call.arguments['type']} ${call.arguments['data']} $error $stackTrace');
+      }
+      return Future.value(null);
+    });
+  }
 }
